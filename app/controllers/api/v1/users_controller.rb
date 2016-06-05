6f6118a7188f2ca56f29293956b34bb157825e30
Jørgen Sundt <jorgen@theeleven.co.uk>
2016-06-05 22:51:16 +0100

    added user create

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,6 +1,6 @@
 module Api::V1
   class UsersController < ApiController
-    before_action :authenticate_v1_user!, only: [:index]
+    before_action :authenticate_v1_user!, only: [:index, :show]
     before_action :set_user, only: [:show]
 
     # GET /v1/users
@@ -12,11 +12,26 @@ module Api::V1
       render json: @user
     end
 
+    def create
+      @user = User.new(user_params)
+
+      if @user.save
+        render json: @user, status: :created, location: @user
+      else
+        render json: @user.errors, status: :unprocessable_entity
+      end
+    end
+
+
 
     private
 
     def set_user
       @user = User.find(params[:id])
+    end
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
     end
   end
 end
